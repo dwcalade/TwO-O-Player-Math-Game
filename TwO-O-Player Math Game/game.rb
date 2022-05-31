f4266e4110f315186aa5c50e7fbc0cc5099adc42
dwcalade <dwcalade@yahoo.ca>
2022-05-30 23:56:47 -0700

    Display Winner

--- TwO-O-Player Math Game/game.rb
+++ TwO-O-Player Math Game/game.rb
@@ -1,32 +1,42 @@
 class Game
-    attr_reader :name, :player_1, :player_2
-    def initialize(name)
-      @name = name
-      @player_1 = Player.new('Player 1')
-      @player_2 = Player.new('Player 2')
-    end
-  
-    def start
-      puts "New Game Started. Welcome #{player_1.name} and #{player_2.name}"
-      pp @player_1
-      pp @player_2
-      turn
-    end
-  
-    def show_stats
-      puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
-    end
-  
-    def turn
-      @player_1.new_question
-      @player_2.new_question
-      if @player_1.lives > 0 && @player_2.lives > 0
-        show_stats
-        puts '---------NEW-TURN---------'
-        turn
-      else
-        puts '---------GAME-OVER--------'
-        puts 'Good bye!'
-      end
+  attr_reader :name, :player_1, :player_2
+  def initialize(name)
+    @name = name
+    @player_1 = Player.new('Player 1')
+    @player_2 = Player.new('Player 2')
+  end
+
+  def start
+    puts "New Game Started. Welcome #{player_1.name} and #{player_2.name}"
+    turn
+  end
+
+  def show_stats
+    puts "P1: #{@player_1.lives}/3 vs P2: #{@player_2.lives}/3"
+  end
+
+  def winner(player)
+    puts "#{player.name} wins with the score of #{player.lives}/3"
+    puts '---------GAME-OVER--------'
+    puts 'Good bye!'
+    exit(0)
+  end
+
+  def check_score
+    if @player_1.lives == 0
+      winner(player_2)
+    elsif @player_2.lives == 0
+      winner(player_1)
     end
   end
+
+  def turn
+    @player_1.new_question
+    check_score
+    @player_2.new_question
+    check_score
+    show_stats
+    puts '---------NEW-TURN---------'
+    turn
+  end
+end
